a8056528dec66931c4c7666bf2619eb587bb025a
Dave Miyazaki <denixden2818@gmail.com>
2026-01-13 18:29:55 -0500

    Cleaned unnecessary code

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -7,13 +7,12 @@ interface ListItemInterface {
   completed: boolean,
 }
 export default function ListItem({id, title, completed}: ListItemInterface){
-  const [spanClicked, setSpanClicked] = useState(false)
   function handleItemListClick(){
     console.log("item was clicked") 
   }
   return(
     <div className="__toDoItem">
-      <input onClick={handleItemListClick} type="text" id={id} value={title} readOnly/>
+      <input onClick={handleItemListClick} type="text" value={title} readOnly/>
       <span>{completed ? "Checked" : "Unchecked"}</span>
       <DeleteButton id={id}/>
     </div>

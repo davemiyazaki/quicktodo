57369dbd61bfdebe6805b882b1ce8df8f8b59d44
Dave Miyazaki <denixden2818@gmail.com>
2026-01-13 16:00:21 -0500

    Add little of style to buttons

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,5 @@
 export default function Button ({id=0, title="Button"}){
   return(
-    <button key={id} type="button" className="">{title}</button>
+    <button key={id} type="button" className="hover:text-red-400 border-solid border-black border-2 p-0.5 ml-0.5 active:text-blue-300">{title}</button>
   )
 }  

09feaa9b7cb130250196bf376fef7c11f14244ef
Dave Miyazaki <denixden2818@gmail.com>
2026-01-13 12:58:51 -0500

    creeated actions.tsx for server actions

--- app/actions.tsx
+++ app/actions.tsx
@@ -1,1 +1,11 @@
 'use client'
+import { createClient } from "@/utils/supabase/server" //importing way to talk to Supabase
+import {cookies} from 'next/headers' // importing function that woudl let us get cookies from browser 
+                                      // cookies help to authenticate with Database - ???
+
+
+function addToDo(title: string ){ //intend use --> addToDo(toDoName);
+
+const supabase = createClient(cookies()) //creating variable that would represent our database
+
+}

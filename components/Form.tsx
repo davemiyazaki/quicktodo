04b827a67bea544be4b07c69c30550fc61e4ab4c
Dave Miyazaki <denixden2818@gmail.com>
2026-01-13 13:30:46 -0500

    added handleSubmit for Form.tsx

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,20 +1,26 @@
 'use client'
 import { useState } from "react";
+import {addToDo} from '@/app/actions'
 export default function Form(){
   const [name, setName] = useState("")
 
   function handleChange(e:any) {
     setName(e.target.value);
   }
+  function handleSubmit(e:any){
+    console.log("Submitted successfully!")
+  }
   return(
-    <form> 
-      <label>New To-Do:
-      <input 
-        type='text'
-        value = {name}
-        onChange={handleChange}
-      />
-      </label>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}> 
+        <label>New To-Do:
+        <input 
+          type='text'
+          value = {name}
+          onChange={handleChange}
+        />
+        </label>
+      </form>
+    </>
   )
  }

deae4c7d13324ff07d559d4d5f3ac7b20c36b20b
Dave Miyazaki <denixden2818@gmail.com>
2026-01-13 18:01:20 -0500

    Added test methods to handle clicks for span and list item

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,27 @@ import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
 import  Form  from '@/components/Form'
 import ListItem from '@/components/ListItem'
-  export default async function Page() {
-  const cookieStore = await cookies()
-  const supabase = createClient(cookieStore)
 
-  const { data: todos } = await supabase.from('todos').select()
+export default async function Page() {
+const cookieStore = await cookies()
+const supabase = createClient(cookieStore)
 
-  return (
-    <div className="__main">
-      <Form />
-      <ul className="__toDoList">
-        {todos?.map((todo) => (
-          <ListItem 
-            key={todo.id}
-            id={todo.id} 
-            title={todo.title} 
-            completed={todo.completed}/>
-        ))}
-      </ul>
-    </div>
-  )
+const { data: todos } = await supabase.from('todos').select()
+
+return (
+  <div className="__main">
+    <Form />
+    <ul className="__toDoList">
+      {todos?.map((todo) => (
+        <ListItem 
+          key={todo.id}
+          id={todo.id} 
+          title={todo.title} 
+          completed={todo.completed}/>
+      ))}
+    </ul>
+  </div>
+)
 }
 
 
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,13 +1,18 @@
+'use client'
 import DeleteButton from "./DeleteButton"
+import { useState } from "react"
 interface ListItemInterface {
   id: number,
   title: string,
   completed: boolean,
 }
 export default function ListItem({id, title, completed}: ListItemInterface){
+  const [spanClicked, setSpanClicked] = useState(false)
+  function handleItemListClick(){spanClicked ? console.log("Span was clicked"):console.log("Span was not clicked"); setSpanClicked(false)}
+  function handleSpanClick(){setSpanClicked(true)}
   return(
-          <li>
-          <span>{title}--{completed ? "Checked" : "Unchecked"}</span>
+          <li onClick={handleItemListClick}>
+          <span onClick={handleSpanClick}>{title}--{completed ? "Checked" : "Unchecked"}</span>
           <DeleteButton id={id} title="Delete" />
           </li>
   ) 

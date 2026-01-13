0eacc4e586aa4673e41fd8a5ca57a4c79e9f1b17
Dave Miyazaki <denixden2818@gmail.com>
2026-01-13 16:59:32 -0500

    Added handleClick for DeleteButton component

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,6 +1,15 @@
 import { deleteToDo } from "@/app/actions"
-export default function DeleteButton ({id=0, title="Button"}){
+export default function DeleteButton ({id=0, title="Delete"}){
+
+  async function handleClick (id: number):Promise<void>{
+   try{
+     await deleteToDo(id);
+   }catch(error){
+    console.log("Failed to delete item ---", error);
+   }
+  }
+
   return(
-    <button onClick={deleteToDo}key={id} type="button" className="hover:text-red-400 border-solid border-black border-2 p-0.5 ml-0.5 active:text-blue-300">{title}</button>
+    <button onClick={handleClick} key={id} type="button" className="hover:text-red-400 border-solid border-black border-2 p-0.5 ml-0.5 active:text-blue-300">{title}</button>
   )
 }  

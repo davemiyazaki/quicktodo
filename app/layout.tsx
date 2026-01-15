a10938d080786eaeb7b7d1c40f59da15e0537d33
Dave Miyazaki <denixden2818@gmail.com>
2026-01-15 15:08:06 -0500

    applied Chiru font thru font-family in global.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={` antialiased`}
+        className={`antialiased`}
       >
         {children}
       </body>

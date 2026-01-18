d0fead134581c135770759439fc7d5bd0a8a52e6
Dave Miyazaki <denixden2818@gmail.com>
2026-01-18 14:58:37 -0500

    Added  styling to Navigation bar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,9 +2,9 @@ import Logo from "./Logo";
 import SettingsBtn from "./SettingsBtn";
 export default function Nav(){
   
-  return(<div>
-         <Logo />
-         "Navigation bar"
-         <SettingsBtn />
+  return(<div className="self-stretch inline-flex justify-center items-start gap-2.5">
+           <Logo />
+           "Navigation bar"
+           <SettingsBtn />
          </div>);
 }

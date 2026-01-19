86dde3157dce88b934ea332751a656e449c148be
Dave Miyazaki <denixden2818@gmail.com>
2026-01-18 19:42:16 -0500

    Styled todolist component

--- components/ToDoList.tsx
+++ components/ToDoList.tsx
@@ -1,7 +1,7 @@
 import ListItem from "./ListItem"
 export default function ToDoList({toDoItems}){
   return(
-    <div>
+    <div className="flex flex-col justify-start items-start gap-2">
         {toDoItems?.map((todo) => (
           <ListItem 
             key={todo.id}

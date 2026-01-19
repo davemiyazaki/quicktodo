7c74d59adb70471344b1037f8b6379f249528053
Dave Miyazaki <denixden2818@gmail.com>
2026-01-19 16:13:53 -0500

    Changed style to w-fit for ToDoList component

--- components/todo/ToDoList.tsx
+++ components/todo/ToDoList.tsx
@@ -1,7 +1,7 @@
 import ToDoItem from "./ToDoItem/ToDoItem"
 export default function ToDoList({toDoItems}){
   return(
-    <div className="flex flex-col justify-center items-start gap-2 w-full">
+    <div className="flex flex-col justify-center items-start gap-2 w-fit">
         {toDoItems?.map((todo) => (
           <ToDoItem 
             key={todo.id}

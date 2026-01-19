eb710a4895f7550e254619e569608e2d20c1bfca
Dave Miyazaki <denixden2818@gmail.com>
2026-01-19 16:11:25 -0500

    set justify-center for content component

--- components/Content.tsx
+++ components/Content.tsx
@@ -3,7 +3,7 @@ import ToDoList from './todo/ToDoList';
 export default function Content({toDoItems}){
 
   return (
-    <div className="inline-flex flex-col justify-start items-start gap-2.5">
+    <div className="inline-flex flex-col justify-center items-start gap-2.5">
       <ToDoList toDoItems={toDoItems}/>
       <InputField />
     </div>

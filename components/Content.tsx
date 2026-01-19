e19bdd50c43e9b5414df95e35c0a14332c2028d7
Dave Miyazaki <denixden2818@gmail.com>
2026-01-18 19:14:11 -0500

    Imported ToDoList component into Content component

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,10 +1,11 @@
 import  Form  from '@/components/Form'
-
+import ToDoList from './ToDoList';
 export default function Content(){
 
   return (
     <div className=''>
       <Form />
+      <ToDoList />
     </div>
   );
 }

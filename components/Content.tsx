0041b3db071a9480b8800e7f814cee5f8deeb8e9
Dave Miyazaki <denixden2818@gmail.com>
2026-01-18 19:30:29 -0500

    Swapped input and item order; Styled to Content

--- components/Content.tsx
+++ components/Content.tsx
@@ -3,9 +3,9 @@ import ToDoList from './ToDoList';
 export default function Content({toDoItems}){
 
   return (
-    <div className=''>
-      <Form />
+    <div className="flex flex-col justify-start items-start gap-2.5">
       <ToDoList toDoItems={toDoItems}/>
+      <Form />
     </div>
   );
 }

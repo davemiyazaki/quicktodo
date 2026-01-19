63ffc8a8fcc7106aa2c7a14ced4a6c63e25249d1
Dave Miyazaki <denixden2818@gmail.com>
2026-01-18 20:31:50 -0500

    Renamed Nav.tsx to NavBar.tsx

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import Logo from "./Logo";
 import SettingsBtn from "./SettingsBtn";
-export default function Nav(){
+export default function NavBar(){
   
   return(<div className="self-stretch inline-flex justify-center items-start gap-2.5">
            <Logo />

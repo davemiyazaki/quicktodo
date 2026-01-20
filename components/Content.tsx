4da1fcb07d553123a4c3b356b6e326bc42b8ca37
Dave Miyazaki <denixden2818@gmail.com>
2026-01-19 20:00:16 -0500

    Aligned content horizontally to the center

--- components/Content.tsx
+++ components/Content.tsx
@@ -3,9 +3,9 @@ import ToDoList from './todo/ToDoList';
 export default function Content({toDoItems}){
 
   return (
-    <div className="inline-flex flex-col justify-center items-start gap-2.5">
+    <div className="flex flex-col justify-start items-center gap-2.5">
       <ToDoList toDoItems={toDoItems}/>
-      <InputField />
+      <InputField /> 
     </div>
   );
 }

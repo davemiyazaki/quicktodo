bec38d8b993214b37a0be7c763d6e18232b66db7
Dave Miyazaki <denixden2818@gmail.com>
2026-01-21 11:48:50 -0500

    Changed alignment for navbar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import Logo from "./Logo";
 import SettingsBtn from "./SettingsBtn";
 export default function NavBar(){
   
-  return(<div className="self-stretch inline-flex justify-center items-start gap-2.5">
+  return(<div className="self-stretch inline-flex justify-center items-center gap-2.5">
            <Logo />
            <SettingsBtn />
          </div>);
--- components/NavBar/SettingsBtn.tsx
+++ components/NavBar/SettingsBtn.tsx
@@ -1,4 +1,4 @@
 export default function SettingsBtn(){
 
-  return(<div className="w-12 h-12 bg-violet-300"/>)
+  return(<div className="w-8 h-8 bg-violet-300"/>)
 }

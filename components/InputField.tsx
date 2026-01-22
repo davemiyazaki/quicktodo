d5ca1588c1a31175b09bd0bd583a666de0d119e2
Dave Miyazaki <denixden2818@gmail.com>
2026-01-22 15:40:22 -0500

    Remove outline on focus input

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -24,7 +24,7 @@ export default function InputField(){
           placeholder="New Task"
           value = {name}
           onChange={handleChange}
-          className="placeholder-gray-400  h-full"
+          className="placeholder-gray-400  h-full focus:outline-none"
         />
         </label>
       </form>
--- components/todo/ToDoItem/ToDoName.tsx
+++ components/todo/ToDoItem/ToDoName.tsx
@@ -1,7 +1,7 @@
 export default function ToDoName({...props}){
   return(
       <input
-        className="w-full h-auto text-blue-950"
+        className="w-full h-auto text-blue-950 focus:outline-none"
         {...props}
         />
   )

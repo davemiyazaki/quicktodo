814f164ccb54b54af198264e061cc935ddc14d25
ミヤザキ <denixden2818@gmail.com>
2026-02-02 20:42:19 -0500

    feat: set useState for todos
    - make todos a state variable
    - fill the array inside of useEffect
    - currenlty returns one test todo item

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@
 //import { createClient } from '@/utils/supabase/server'
 //import { cookies } from 'next/headers'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import NavBar from '@/components/NavBar/NavBar'
 import Content from '@/components/Content'
 import * as local from '@/utils/local'
+import { ToDoItemType } from '@/types/common'
 
 export default function Page() {
 
@@ -17,10 +18,12 @@ export default function Page() {
 //const data = JSON.parse(localStorage.getItem('todos'));
 
 //const initialData: Item[] = [{id:1, title:"todo",isCompleted:false}]; 
-const todos = local.read();
+const [todos, setTodos] = useState<ToDoItemType>(); 
 useEffect(()=>{
+  setTodos(local.read())
   console.log(todos);
 }, [])
+
 return (
     <>
       <NavBar />
